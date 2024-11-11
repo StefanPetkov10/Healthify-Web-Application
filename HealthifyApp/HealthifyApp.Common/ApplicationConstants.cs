2e22f6fd509111005c81252885b619a0b842139f
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-11-11 23:11:11 +0200

    Add ApplicationConstants

---
+++ HealthifyApp/HealthifyApp.Common/ApplicationConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace CinemaApp.Common
+{
+    public static class ApplicationConstants
+    {
+        public const int releaseYear = 2024;
+
+        public const string noImageUrl = "/images/no-image.jpg";
+    }
+}
