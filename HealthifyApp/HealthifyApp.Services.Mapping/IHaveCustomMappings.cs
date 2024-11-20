549378eb8a47dbbbb21fbed89a4ff16d0f2ebad4
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-11-20 21:45:22 +0200

    Add interface IHaveCustomMappings

---
+++ HealthifyApp/HealthifyApp.Services.Mapping/IHaveCustomMappings.cs
@@ -0,0 +1,9 @@
+﻿using AutoMapper;
+
+namespace HealthifyApp.Services.Mapping
+{
+    public interface IHaveCustomMappings
+    {
+        void CreateMappings(IProfileExpression configurate);
+    }
+}
